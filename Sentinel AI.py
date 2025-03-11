ae929e4d84f9de3aa18f19f5338428ef8c0b64c8
hberbecia <hberbecia@uc.edu.ve>
2025-03-11 16:51:15 -0400

    Create Sentinel AI.py

---
+++ Sentinel AI.py
@@ -0,0 +1,7 @@
+import customtkinter as ctk
+import pywinstyles
+from PIL import Image
+import os 
+
+base_dir = os.pata.dirname(os.path.abspath(__file__))
+resources_folder = os.path.join (base_dir, "resources")
