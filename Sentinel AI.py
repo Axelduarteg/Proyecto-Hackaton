020467a0a0e3d1f42c9d10d486b5aa796bf94623
AxelDuarte <102534652+Axelduarteg@users.noreply.github.com>
2025-03-11 21:52:22 +0100

    Update Sentinel AI.py

--- Sentinel AI.py
+++ Sentinel AI.py
@@ -5,3 +5,10 @@ import os
 
 base_dir = os.pata.dirname(os.path.abspath(__file__))
 resources_folder = os.path.join (base_dir, "resources")
+
+app = ctk.CTk()
+app.geometry("600x400")
+app.title("SentinelAI")
+pywinstyles.apply_style(window=app, style="aero")
+ctk.set_appearance_mode("system")
+ctk.set_default_color_theme("blue")
